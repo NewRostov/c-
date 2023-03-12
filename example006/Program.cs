c9f98e72e6f16fe794d7d7a80868ccc6ab31d0f5
NewRostov <kim.sergey.rostov@gmail.com>
2023-03-12 19:56:14 +0100

    задача 6, условие if-else гири

---
+++ example006/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 8;
+int b = 18;
+int c = 28;
+int d = 58;
+int e = 28;
+int Max = a;
+if (b > Max) Max = b;
+if (c > Max) Max = c;
+if (d > Max) Max = d;
+if (e > Max) Max = e;
+Console.WriteLine("Max = " + Max);
