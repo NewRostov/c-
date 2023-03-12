f5350adbc45ccd70e4ed9fb3b597694d749a2377
NewRostov <kim.sergey.rostov@gmail.com>
2023-03-12 18:31:16 +0100

    задача 5 ветвление

---
+++ example005/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите имя: ");
+string name = Console.ReadLine();
+if (name == "Маша")
+{ Console.WriteLine("Ура это же " + name); }
+else Console.WriteLine("Привет: " + name);
+
