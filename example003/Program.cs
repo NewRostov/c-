6874f054e6145fefba00a321c09909db8d8dfc96
NewRostov <kim.sergey.rostov@gmail.com>
2023-03-12 18:18:24 +0100

    сложение случайных чисел

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,3 +1,5 @@
-﻿int a = 20;
-int b = 40;
-Console.WriteLine( a + b);
+﻿int a = new Random().Next(1, 9);
+int b = new Random().Next(1, 9);
+Console.WriteLine("число a: " + a);
+Console.WriteLine("число b: " + b);
+Console.WriteLine("сумма чисел: a + b = " + a + b);
